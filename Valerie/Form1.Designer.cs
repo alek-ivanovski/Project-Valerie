fffa57471b4a8e28c183d529e548d6d09a2eaa14
Alek Ivanovski <alek.ivanovski96@gmail.com>
2017-04-25 20:55:56 +0200

    Ver 1.0

---
+++ Valerie/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Valerie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtEncryption = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEncryption
+            // 
+            this.txtEncryption.Location = new System.Drawing.Point(13, 13);
+            this.txtEncryption.Multiline = true;
+            this.txtEncryption.Name = "txtEncryption";
+            this.txtEncryption.Size = new System.Drawing.Size(311, 182);
+            this.txtEncryption.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(13, 201);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(249, 201);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg.Location = new System.Drawing.Point(120, 209);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 15);
+            this.lblMsg.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEncrypt;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(336, 228);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtEncryption);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Project Valerie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEncryption;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ Valerie/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Valerie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if(txtEncryption == null)
+            {
+                lblMsg.Text = "Required Field *";
+            }
+            else
+            {
+                txtEncryption.Text = Encrypt(txtEncryption.Text);
+                this.AcceptButton = btnDecrypt;
+            }
+        }
+
+        //Encrypt function takes the string from the TextBox, turns it into byte array and adds 85 to each byte.
+        //Function returns a string of the encrypted text.
+        private string Encrypt (string text)
+        {
+            byte[] toBytes = Encoding.ASCII.GetBytes(text); 
+            string s = "";
+            foreach (byte b in toBytes)
+            {
+                int tmp = Convert.ToInt32(b);
+                tmp += 85;
+                s += tmp + " ";
+            }
+            return s;
+        }
+
+        //Decrypt does the opposite of Encrypt and returns the decrypted message.
+        private string Decrypt (string text)
+        {
+            string message = "";
+            string[] chars = text.Split(' ');
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                int tmp = Int32.Parse(chars[i]);
+                tmp -= 85;
+                message += ((char)tmp).ToString();
+            }
+            return message;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (txtEncryption == null)
+            {
+                lblMsg.Text = "Required Field *";
+            }
+            else
+            {
+                txtEncryption.Text = Decrypt(txtEncryption.Text);
+                this.AcceptButton = btnEncrypt;
+            }
+        }
+    }
+}
